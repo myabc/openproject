5bc10c6e1bcc61132544375b9acbd7feed6c50ca
Alexander Brandon Coles <alex@alexbcoles.com>
2025-09-23 16:21:50 +0100

    Revert "Disable eslint no-unused-vars rule for .d.ts files"
    This reverts commit 61e36dc56abfc16529bc945a892fb747555ffd64.

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -155,13 +155,6 @@ export default defineConfig([
     }
   },
   {
-    files: ['**/*.d.ts'],
-    rules: {
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-    },
-  },
-  {
     files: ['**/*.spec.ts'],
     plugins: { jasmine },
     extends: [
